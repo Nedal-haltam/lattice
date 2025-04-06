f35d48918c1da944d0e9220d64dbb11d41b69226
Nedal Abu Haltam <nedalhaltam1@gmail.com>
2025-04-06 13:42:52 +0300

    construct a lattice

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿
 using Raylib_cs;
+using System.Numerics;
 
 namespace lattice
 {
@@ -7,14 +8,44 @@ namespace lattice
     {
         static void Main(string[] args)
         {
+            int w = 1600;
+            int h = 800;
             Raylib.SetConfigFlags(ConfigFlags.AlwaysRunWindow | ConfigFlags.ResizableWindow);
             Raylib.SetTargetFPS(60);
-            Raylib.InitWindow(800, 600, "Complex");
+            Raylib.InitWindow(w, h, "lattice");
 
+            List<Vector2> basisVec = 
+                [
+                    new(1, 0), 
+                    new(0, 1),
+                ];
+            List<Vector2> set = [];
+            int N = 20;
+            float r = 3.0f;
+            Color c = Color.White;
+            int scale = 10;
+            for (int i = -N; i < N; i++)
+            {
+                for (int j = -N; j < N; j++)
+                {
+                    set.Add(scale * (i * basisVec[0] + j * basisVec[1]) + new Vector2(w / 2, h / 2));
+                }
+            }
             while (!Raylib.WindowShouldClose())
             {
                 Raylib.BeginDrawing();
                 Raylib.ClearBackground(new(0x18, 0x18, 0x18));
+
+
+
+                for (int i = 0; i < set.Count(); i++)
+                {
+                    Raylib.DrawCircleV(set[i], r, c);
+                }
+
+
+
+
                 Raylib.DrawFPS(0, 0);
                 Raylib.EndDrawing();
             }
