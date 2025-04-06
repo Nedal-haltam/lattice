b70869e8b2d00aab7cea3610828dbac6fb9a18a5
Nedal Abu Haltam <nedalhaltam1@gmail.com>
2025-04-06 12:29:55 +0300

    add raylib

--- Program.cs
+++ Program.cs
@@ -1,10 +1,24 @@
-﻿namespace lattice
+﻿
+using Raylib_cs;
+
+namespace lattice
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Raylib.SetConfigFlags(ConfigFlags.AlwaysRunWindow | ConfigFlags.ResizableWindow);
+            Raylib.SetTargetFPS(60);
+            Raylib.InitWindow(800, 600, "Complex");
+
+            while (!Raylib.WindowShouldClose())
+            {
+                Raylib.BeginDrawing();
+                Raylib.ClearBackground(new(0x18, 0x18, 0x18));
+                Raylib.DrawFPS(0, 0);
+                Raylib.EndDrawing();
+            }
+            Raylib.CloseWindow();
         }
     }
 }
